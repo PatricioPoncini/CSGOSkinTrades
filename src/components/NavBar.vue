1a70cfa0c5dd3b0c5a193b7660a2eeb2e18028d8
PatricioPoncini <patricioponcini10@gmail.com>
2024-06-29 14:54:34 -0300

    feat: home view

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
 
 <nav class="bg-white border-gray-200">
   <div class="max-w-screen bg-[#e38720] flex flex-wrap items-center justify-between mx-auto p-4">
-    <span class="self-center text-2xl text-black hover:text-white font-semibold whitespace-nowrap cursor-pointer transition duration-500">CSGO Skin Trades</span>
+    <a href="/" class="self-center text-2xl text-black hover:text-white font-semibold whitespace-nowrap cursor-pointer transition duration-500">CSGO Skin Trades</a>
     <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,16 @@
 <template>
-    <p class="text-red-500 hover:text-blue-500 transition duration-300">Hello world</p>
-</template>
+    <div class="h-screen relative overflow-hidden">
+      <video autoplay loop muted playsinline class="absolute inset-0 w-full h-full object-cover">
+        <source src="/homevideo.mp4" type="video/mp4" />
+      </video>
+      <div class="absolute inset-0 bg-black/50 flex items-center justify-center px-4 text-center">
+        <div class="space-y-4">
+          <h1 class="text-4xl font-bold text-white sm:text-5xl md:text-6xl relative z-10">Bienvenidos a CS GO Skin Trades</h1>
+          <p class="text-lg text-white md:text-xl relative z-10">
+            Descubre el mundo del mercado más grande de Steam
+          </p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
