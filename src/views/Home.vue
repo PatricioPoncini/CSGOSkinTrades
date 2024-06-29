5ff7e87dfe590aeff4c282c78e49f6a7201b12c2
PatricioPoncini <patricioponcini10@gmail.com>
2024-06-29 15:38:20 -0300

    feat: home fig

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="h-screen relative overflow-hidden">
-      <video autoplay loop muted playsinline class="absolute inset-0 w-full h-full object-cover">
-        <source src="/homevideo.mp4" type="video/mp4" />
-      </video>
+      <img src="/homegif.gif" alt="Background GIF" class="absolute inset-0 w-full h-full object-cover" />
       <div class="absolute inset-0 bg-black/50 flex items-center justify-center px-4 text-center">
         <div class="space-y-4">
           <h1 class="text-4xl font-bold text-white sm:text-5xl md:text-6xl relative z-10">Bienvenidos a CS GO Skin Trades</h1>
